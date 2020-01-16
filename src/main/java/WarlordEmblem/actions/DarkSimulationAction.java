66e43fdf2254f8081d87df9923a50d94f4329468
Rita <13536709069@163.com>
2020-01-16 15:47:51 +0800

    --调整了符文巨剑的最大符文能量显示位置 --凛风打击 更名为 凛风冲击  ;冷酷严冬 更名 凌风打击 --符文集中回复符文能量3（4）>1(2) --修复了吉恩.格雷迈恩 id错误的问题 --符文重铸弃 1（2）张牌> 消耗 1 张牌 --新增 鲜血祭祀 冰疫打击 黑暗模拟

---
+++ src/main/java/WarlordEmblem/actions/DarkSimulationAction.java
@@ -0,0 +1,57 @@
+ package WarlordEmblem.actions;
+ import com.megacrit.cardcrawl.actions.AbstractGameAction;
+ import com.megacrit.cardcrawl.cards.AbstractCard;
+ import com.megacrit.cardcrawl.core.Settings;
+ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+ import com.megacrit.cardcrawl.helpers.CardLibrary;
+ import com.megacrit.cardcrawl.screens.CardRewardScreen;
+ import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToDiscardEffect;
+ import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToHandEffect;
+
+ import java.util.ArrayList;
+ 
+ public class DarkSimulationAction extends AbstractGameAction {
+   private boolean retrieveCard;
+   private ArrayList<AbstractCard> cards ;
+
+   public DarkSimulationAction(ArrayList<AbstractCard> Cards) {
+     this.retrieveCard = false;
+     this.cards = Cards;
+
+     this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
+     this.duration = Settings.ACTION_DUR_FAST;
+
+   }
+
+   
+   public void update() {
+       if(AbstractDungeon.getCurrRoom().isBattleEnding()){
+           this.isDone = true;
+           return;
+       }
+
+     if (this.duration == Settings.ACTION_DUR_FAST) {
+       AbstractDungeon.cardRewardScreen.customCombatOpen(cards, CardRewardScreen.TEXT[1], true);
+       tickDuration();
+       return;
+     }
+     if (!this.retrieveCard) {
+       if (AbstractDungeon.cardRewardScreen.discoveryCard != null) {
+         AbstractCard disCard = AbstractDungeon.cardRewardScreen.discoveryCard.makeStatEquivalentCopy();
+           disCard.setCostForTurn(0);
+         disCard.current_x = -1000.0F * Settings.scale;
+         if (AbstractDungeon.player.hand.size() < 10) {
+           AbstractDungeon.effectList.add(new ShowCardAndAddToHandEffect(disCard, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+         }
+         else {
+           AbstractDungeon.effectList.add(new ShowCardAndAddToDiscardEffect(disCard, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+         }
+         AbstractDungeon.cardRewardScreen.discoveryCard = null;
+       } 
+       this.retrieveCard = true;
+     }
+     tickDuration();
+   }
+ }
+
+
---
+++ src/main/java/WarlordEmblem/cards/DeathKnight/DarkSimulation.java
@@ -0,0 +1,179 @@
+package WarlordEmblem.cards.DeathKnight;
+
+import WarlordEmblem.WarlordEmblem;
+import WarlordEmblem.actions.DarkSimulationAction;
+import WarlordEmblem.patches.CardColorEnum;
+import WarlordEmblem.patches.CustomTagsEnum;
+import basemod.helpers.BaseModCardTags;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.watcher.ChooseOneAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class DarkSimulation extends AbstractDKCard {
+    public static final String ID = WarlordEmblem.makeID("DarkSimulation");
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String IMG = WarlordEmblem.assetPath("img/cards/DeathKnight/Skill.png");
+    private static final int COST = 1;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final CardType TYPE = CardType.SKILL;
+    private static final CardColor COLOR = CardColorEnum.DeathKnight_LIME;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    public static final Logger logger = LogManager.getLogger(WarlordEmblem.class.getSimpleName());
+
+
+    public DarkSimulation() {
+        super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = 3;
+        this.magicNumber = this.baseMagicNumber;
+        this.exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        ArrayList<AbstractCard> RealmCardChoices = new ArrayList<AbstractCard>();
+        ArrayList<AbstractCard.CardTags> CardTypeMark = new ArrayList<CardTags>();
+
+        if (hasBloodRealm()) {
+            CardTypeMark.add(CustomTagsEnum.Blood_Realm_Tag);
+        }
+        if (hasIceRealm()) {
+            CardTypeMark.add(CustomTagsEnum.Ice_Realm_Tag);
+        }
+        if (hasEvilRealm()) {
+            CardTypeMark.add(CustomTagsEnum.Evil_Realm_Tag);
+        }
+
+        if(CardTypeMark.size() != 0){
+            addToBot(new DarkSimulationAction(returnRandomRealmCardInCombat(CardTypeMark)));
+     }
+    }
+
+
+
+    public static ArrayList<AbstractCard> returnRandomRealmCardInCombat(ArrayList<AbstractCard.CardTags> tags) {
+           ArrayList<AbstractCard> list = new ArrayList<AbstractCard>();
+           ArrayList<AbstractCard> returnCard = new ArrayList<AbstractCard>();
+
+           if(tags.contains(CustomTagsEnum.Blood_Realm_Tag)){
+               for (AbstractCard c : AbstractDungeon.srcCommonCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Blood_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+               for (AbstractCard c : AbstractDungeon.srcUncommonCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Blood_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+               for (AbstractCard c : AbstractDungeon.srcRareCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Blood_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+           }
+           if(tags.contains(CustomTagsEnum.Ice_Realm_Tag)){
+               for (AbstractCard c : AbstractDungeon.srcCommonCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Ice_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+               for (AbstractCard c : AbstractDungeon.srcUncommonCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Ice_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+               for (AbstractCard c : AbstractDungeon.srcRareCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Ice_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+           }
+           if(tags.contains(CustomTagsEnum.Evil_Realm_Tag)){
+               for (AbstractCard c : AbstractDungeon.srcCommonCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Evil_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+               for (AbstractCard c : AbstractDungeon.srcUncommonCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Evil_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+               for (AbstractCard c : AbstractDungeon.srcRareCardPool.group) {
+                   if (c.hasTag(CustomTagsEnum.Evil_Realm_Tag)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                       list.add(c);
+                   }
+               }
+           }
+
+            int temp;
+           for(int i = 0;i < 3;i++){
+               temp = AbstractDungeon.cardRng.random(list.size() - 1);
+               returnCard.add(list.get(temp));
+               list.remove(temp);
+           }
+
+           return returnCard;
+       }
+
+
+       /*
+    public AbstractCard getRandomRealmCard(boolean useRng,AbstractCard.CardTags tags) {
+        ArrayList<AbstractCard> tmp = new ArrayList();
+
+        for (AbstractCard c : AbstractDungeon.srcCommonCardPool.group) {
+            if (c.hasTag(tags)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                tmp.add(c);
+            }
+        }
+        for (AbstractCard c : AbstractDungeon.srcUncommonCardPool.group) {
+            if (c.hasTag(tags)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                tmp.add(c);
+            }
+        }
+        for (AbstractCard c : AbstractDungeon.srcRareCardPool.group) {
+            if (c.hasTag(tags)  && !c.hasTag(AbstractCard.CardTags.HEALING)) {
+                tmp.add(c);
+            }
+        }
+
+        if (tmp.isEmpty()) {
+            logger.info("ERROR: No cards left for type: " + this.type.name());
+            return null;
+        } else {
+            Collections.sort(tmp);
+            if (useRng) {
+                return (AbstractCard)tmp.get(AbstractDungeon.cardRng.random(tmp.size() - 1));
+            } else {
+                return (AbstractCard)tmp.get(MathUtils.random(tmp.size() - 1));
+            }
+        }
+    }
+
+*/
+
+        public AbstractCard makeCopy() { return new DarkSimulation(); }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+
+}
